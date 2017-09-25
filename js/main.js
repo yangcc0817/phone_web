83460e86c19bac11c086c8b1bf07b79bcfb848ca
yangcc0817 <yangcc0817@126.com>
2017-09-26 00:18:58 +0800

    基本完成

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@
  * Created by Administrator on 2017/9/16.
  */
 $(document).ready(function (){
-    var freeNum = Math.floor(Math.random()*999+1);
-    $(".form-num").html(freeNum);
+    var numString = "365";
+    var num = parseInt(numString);
+    function setNum(str){
+        var first = str[0];
+        var second = str[1];
+        var third =str[2];
+        $(".header span").html(first);
+        $(".header b").html(second);
+        $(".header i").html(third);
+    }
+    setNum(numString);
 
     //提交报价
     $(".cal-form input[type='submit'] ").click(function(){
-
 
         $.ajax({
             cache: true,
@@ -19,7 +27,8 @@ $(document).ready(function (){
 
             },
             success: function() {
-               freeNum --;
+                num--;
+                setNum(num);
             }
         });
     });

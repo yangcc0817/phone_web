e4b7f979ce2bd0df569b6cd179e7e46ed3f66137
yangcc0817 <yangcc0817@126.com>
2017-10-10 22:13:30 +0800

    增加弹框

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
  */
 $(document).ready(function (){
     var numString = "365";
-    var num = parseInt(numString);
     function setNum(str){
         var first = str[0];
         var second = str[1];
@@ -13,24 +12,49 @@ $(document).ready(function (){
         $(".header i").html(third);
     }
     setNum(numString);
-
-    //提交报价
-    $(".cal-form input[type='submit'] ").click(function(){
-
-        $.ajax({
-            cache: true,
-            type: "POST",
-            url:"",
-            data:$('#nameForm').serialize(),
-            async: false,
-            error: function() {
-
-            },
-            success: function() {
-                num--;
-                setNum(num);
-            }
-        });
+    $(".msg-close").click(function(){
+        $("#myModal").modal('hide');
     });
+    $(".PriceModal-close").click(function(){
+        $("#PriceModal").modal('hide');
+    });
+    //预约套餐报价
+    $("#bookPrice").click(function(){
+        var reg = /^1[34578]\d{9}$/;
+        var cusName = $(".customer-name input[name='customerName']").val();
+        var phone = $(".phone-number input[name='customerPhone']").val();
+        if(!cusName) {
+            alert("请先输入姓名！");
+            return;
+        }else if(!phone){
+            alert("请输入手机号码！");
+            return;
+        }
+        var result = reg.test(phone);
+        if(phone && !result){
+            alert("请输入正确的手机号码！");
+            return;
+        }
+        if (phone && result && cusName) {
+            $.ajax({
+                type: "POST",
+                url:"",
+                data:{},
+                async: false,
+                error: function(request) {
+                    alert("Connection error");
+                },
+                success: function(data) {
+                    $("#myModal").modal('hide');
+                    $(".customer-name input[name='customerName']").val("");
+                    $(".phone-number input[name='customerPhone']").val("");
+                    //弹出框
+                   //alert("报价成功，稍后联系您！");
+                }
+            });
+        }
+    });
+
+
 
 });
